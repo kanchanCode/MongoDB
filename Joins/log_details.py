840285404bfbeac5526e4ce2b59db7d5abae119f
kanchan0912 <jkanchan2911@gmail.com>
2022-01-22 10:46:12 +0530

    [Kanchan]Add:log details

---
+++ Joins/log_details.py
@@ -0,0 +1,15 @@
+import logging
+logger=logging
+
+logger.basicConfig(
+filename='C:\\Users\\hp\\Desktop\\MongoDB\\Joins\\log_file.log',
+filemode='w',
+level=logging.INFO,
+format='%(asctime)s:%(funcName)s:%(levelname)s:%(message)s'
+)
+
+logger.basicConfig(
+filename='C:\\Users\\hp\\Desktop\\MongoDB\\Joins\\log_file.log',
+filemode='w',
+format='%(asctime)s:%(funcName)s:%(levelname)s:%(lineno)d'
+)
